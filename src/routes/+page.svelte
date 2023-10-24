0380780c4e5c0515864b274b70d265256450299d
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2023-10-24 17:58:06 +0200

    tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,19 @@
 <script>
-	import "../app.css";
 	import Address from "../components/Address.svelte";
-	import Footer from "../components/Footer.svelte";
-	import Header from "../components/Header.svelte";
 	import Introduction from "../components/Introduction.svelte";
 	import Option from "../components/Option.svelte";
 	import { texts } from "../info/options";
 </script>
 
-<div
-	class="flex flex-row justify-center items-start dark:bg-dark-gray dark:text-white scroll-smooth">
-	<div class="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 dark:bg-dark-gray">
-		<Header />
-		<Introduction />
-		<div class="flex flex-col lg:flex-row flex-wrap">
-			{#each texts as option}
-				<Option
-					title={option.title}
-					optionText={option.text}
-					icon={option.icon}
-					iconWhite={option.iconWhite} />
-			{/each}
-		</div>
-		<Address />
-		<Footer />
+<div>
+	<Introduction />
+	<div class="flex flex-col lg:flex-row flex-wrap">
+		{#each texts as option}
+			<Option
+				title={option.title}
+				optionText={option.text}
+				icon={option.icon}
+				iconWhite={option.iconWhite} />
+		{/each}
 	</div>
 </div>
-
-<style>
-</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	const projects = [
+		{
+			name: "Projekt 1",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl."
+		},
+		{
+			name: "Projekt 2",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl."
+		},
+		{
+			name: "Projekt 3",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni sl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nisl nisl vitae ni."
+		}
+	];
+</script>
+
+<div class="flex flex-col justify-start items-center my-8 gap-8">
+	{#each projects as project}
+		<div class="flex flex-col items-center w-full px-8 gap-4">
+			<div class="flex flex-col items-center md:items-start gap-4 md:flex-row md:justify-between">
+				<img src="/placeholder.jpg" alt="placeholder" class="md:w-1/2 h-auto" />
+				<div class="hidden md:block w-4" />
+				<div class="md:w-1/2">
+					<h2 class="font-bold text-xl text-center md:text-left leading-none mb-3">
+						{project.name}
+					</h2>
+					<p class="text-sm text-center md:text-left">{project.description}</p>
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
