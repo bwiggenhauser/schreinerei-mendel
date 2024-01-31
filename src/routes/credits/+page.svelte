b403304c16d704dc946b2c880fdf4b53201d2c5b
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2024-01-31 18:45:28 +0100

    added datenschutz

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,3 +1,65 @@
-<h1 class="font-bold text-center p-4">
-	Hier werden die Datenschutz-Informationen und die Credits stehen
-</h1>
+<div class="flex flex-col gap-4 px-4 text-sm text-center items-center">
+	<h1 class="font-bold text-center text-2xl mt-4">Datenschutzerklärung</h1>
+	<h2 class="font-bold text-center text-xl">Allgemeiner Hinweis und Pflichtinformation</h2>
+	<h2 class="font-bold text-lg">Benennung der verantwortlichen Stelle</h2>
+	<p>Die verantwortliche Stelle für die Datenverarbeitung auf dieser Website ist:</p>
+	<div>
+		<p>Schreinerei Mendel</p>
+		<p>Niklas Mendel</p>
+		<p>Holbeinstraße 13</p>
+		<p>88212 Ravensburg</p>
+	</div>
+	<p class="md:w-1/2">
+		Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen über die Zwecke und
+		Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.).
+	</p>
+
+	<h2 class="font-bold text-lg mt-8">Widerruf Ihrer Einwilligung zur Datenverarbeitung</h2>
+	<p class="md:w-1/2">
+		Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge der Datenverarbeitung möglich.
+		Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den Widerruf genügt
+		eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit der bis zum Widerruf erfolgten
+		Datenverarbeitung bleibt vom Widerruf unberührt.
+	</p>
+
+	<h2 class="font-bold text-lg mt-8">Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde</h2>
+	<p class="md:w-1/2">
+		Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Verstoßes ein Beschwerderecht
+		bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde bezüglich
+		datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem sich
+		der Sitz unseres Unternehmens befindet. <a
+			href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="font-bold hover:text-mendel-yellow text-mendel-blue transition">Hier</a> wird eine Liste
+		der Datenschutzbeauftragten sowie deren Kontaktdaten bereitgestellt.
+	</p>
+
+	<h2 class="font-bold text-lg mt-8">Recht auf Datenübertragbarkeit</h2>
+	<p class="md:w-1/2">
+		Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung
+		eines Vertrags automatisiert verarbeiten, an sich oder an Dritte aushändigen zu lassen. Die
+		Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern Sie die direkte Übertragung der
+		Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar
+		ist.
+	</p>
+
+	<h2 class="font-bold text-lg mt-8">Recht auf Auskunft, Berichtigung, Sperrung, Löschung</h2>
+	<p class="md:w-1/2">
+		Sie haben jederzeit im Rahmen der geltenden gesetzlichen Bestimmungen das Recht auf
+		unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, Herkunft der Daten,
+		deren Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung,
+		Sperrung oder Löschung dieser Daten. Diesbezüglich und auch zu weiteren Fragen zum Thema
+		personenbezogene Daten können Sie sich jederzeit über die im Impressum aufgeführten
+		Kontaktmöglichkeiten an uns wenden.
+	</p>
+
+	<h2 class="font-bold text-lg mt-8">SSL- bzw. TLS-Verschlüsselung</h2>
+	<p class="md:w-1/2">
+		Aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, die Sie an uns als
+		Seitenbetreiber senden, nutzt unsere Website eine SSL-bzw. TLS-Verschlüsselung. Damit sind
+		Daten, die Sie über diese Website übermitteln, für Dritte nicht mitlesbar. Sie erkennen eine
+		verschlüsselte Verbindung an der „https://“ Adresszeile Ihres Browsers und am Schloss-Symbol in
+		der Browserzeile.
+	</p>
+</div>
