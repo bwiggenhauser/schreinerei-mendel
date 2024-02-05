fe1bd67b10adb2f52ac59a6058d3f9ba910ce88c
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2024-02-05 21:21:01 +0100

    added imprint

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,1 +1,84 @@
-<h1 class="font-bold text-center p-4">Hier wird das Impressum stehen</h1>
+<div class="flex flex-col gap-4 px-4 text-sm text-center items-center">
+	<h1 class="font-bold text-center text-2xl mt-4">Impressum</h1>
+
+	<p>Angaben gemäß §5 TMG</p>
+	<div>
+		<p>Werner Mendel</p>
+		<p>Holbeinstraße 13</p>
+		<p>88212 Ravensburg</p>
+	</div>
+
+	<div>
+		<p class="font-bold">Vertreten durch:</p>
+		<p>Werner Mendel</p>
+	</div>
+
+	<div>
+		<p class="font-bold">Kontakt:</p>
+		<p>Telefon: 0751 - 2 34 79</p>
+		<p>Fax: 0751 - 3662688</p>
+		<p>E-Mail: schreinerei-mendel@t-online.de</p>
+	</div>
+
+	<div>
+		<p class="font-bold">Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz:</p>
+		<p>DE182928402</p>
+	</div>
+
+	<div>
+		<p class="font-bold">Aufsichtsbehörde:</p>
+		<p>Handwerkskammer Ulm/Tischler</p>
+	</div>
+
+	<div>
+		<p class="font-bold text-xl">Haftungsausschluss</p>
+	</div>
+
+	<div>
+		<p class="font-bold text-lg mb-1">Haftung für Inhalte</p>
+		<p>
+			Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit,
+			Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als
+			Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
+			allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch
+			nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+			nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen
+			zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen
+			bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+			Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+			Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+		</p>
+	</div>
+
+	<div>
+		<p class="font-bold text-lg mb-1">Urheberrecht</p>
+		<p>
+			Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
+			deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+			Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+			jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den
+			privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht
+			vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+			Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+			Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+			Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+		</p>
+	</div>
+
+	<div>
+		<p class="font-bold text-lg mb-1">Datenschutz</p>
+		<p>
+			Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich.
+			Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder
+			E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis.
+			Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.Wir weisen
+			darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
+			Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch
+			Dritte ist nicht möglich.Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten
+			Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und
+			Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten
+			behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von
+			Werbeinformationen, etwa durch Spam-Mails, vor.
+		</p>
+	</div>
+</div>
