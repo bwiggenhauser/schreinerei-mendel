9c71fb8985d3136008345150a10e6b52b13465c2
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2024-02-17 19:10:39 +0100

    added image enlarge on click

--- src/components/DetailedInfomation.svelte
+++ src/components/DetailedInfomation.svelte
@@ -1,9 +1,29 @@
-<script>
+<script lang="ts">
 	export let title = "";
 	export let path = "";
 	export let indices = [];
 	const x = "max-w-1/5 md:w-2/5 object-cover shadow-lg rounded-xl hover:scale-125 transition-all";
+
+	import ImageModal from "./ImageModal.svelte";
+
+	let showModal = false;
+	let selectedImage = "";
+
+	function openModal(imageUrl) {
+		console.log(imageUrl);
+		selectedImage = imageUrl;
+		showModal = true;
+	}
+
+	function closeModal() {
+		console.log("CLOSING MODAL");
+		showModal = false;
+	}
 </script>
+
+{#if showModal}
+	<ImageModal imageUrl={selectedImage} {closeModal} />
+{/if}
 
 <div class="">
 	<div class="p-4">
@@ -12,9 +32,10 @@
 	<div class="flex flex-row flex-wrap gap-4 justify-center">
 		{#each indices as index}
 			<img
+				on:click={() => openModal(path + "/" + index + ".jpg")}
 				src={path + "/" + index + ".jpg"}
 				alt="empty"
-				class="md:w-64 md:h-64 h-auto w-full object-cover rounded-lg md:hover:scale-150 md:transition-all" />
+				class="md:w-64 md:h-64 h-auto w-full object-cover rounded-lg md:transition-all" />
 		{/each}
 	</div>
 </div>
---
+++ src/components/ImageModal.svelte
@@ -0,0 +1,31 @@
+<!-- ImageModal.svelte -->
+<script lang="ts">
+	export let imageUrl: string;
+	export let closeModal: () => void;
+</script>
+
+<div class="modal" on:click={closeModal} on:keydown={closeModal} role="button" tabindex="0">
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<div
+		class="modal-content rounded-lg p-1 md:p-4 shadow-xl bg-white w-4/5 md:h-4/5 md:w-fit"
+		on:click|stopPropagation
+		on:keydown|stopPropagation
+		role="dialog">
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<img src={imageUrl} class="object-cover md:h-full rounded-lg" />
+	</div>
+</div>
+
+<style>
+	.modal {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
