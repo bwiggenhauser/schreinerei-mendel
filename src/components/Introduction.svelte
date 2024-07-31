20a70f2be43318188dfd53264b5dc82c7d83dd5c
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2024-07-31 17:41:32 +0200

    add betriebsferien

--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -1,3 +1,8 @@
+<div class="mt-4 bg-mendel-blue py-4 px-8 text-center text-white rounded-lg">
+	<p class="font-bold text-xl">Hinweis</p>
+	<p>Wir sind vom 29.07. - 12.08. in den Betriebsferien.</p>
+</div>
+
 <div class="mt-4 bg-mendel-yellow py-4 px-8 text-center text-black rounded-lg">
 	<p>Herzlich Willkommen bei der Schreinerei Mendel:</p>
 	<p>Ein Familienbetrieb aus Ravensburg in Baden-Württemberg.</p>
