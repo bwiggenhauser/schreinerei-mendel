452303687d61c9098b9ff34ea74401b6740750a3
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2024-08-14 18:59:33 +0200

    remove hinweis

--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -1,7 +1,7 @@
-<div class="mt-4 bg-mendel-blue py-4 px-8 text-center text-white rounded-lg">
+<!-- <div class="mt-4 bg-mendel-blue py-4 px-8 text-center text-white rounded-lg">
 	<p class="font-bold text-xl">Hinweis</p>
 	<p>Wir sind vom 29.07. - 12.08. in den Betriebsferien.</p>
-</div>
+</div> -->
 
 <div class="mt-4 bg-mendel-yellow py-4 px-8 text-center text-black rounded-lg">
 	<p>Herzlich Willkommen bei der Schreinerei Mendel:</p>
